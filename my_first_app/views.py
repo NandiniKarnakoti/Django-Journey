c312c318cdc7d416fa746606323857283b30e758
NandiniKarnakoti <nandinikarnakoti@gmail.com>
2025-08-10 15:10:53 +0530

    Initial commit

--- my_first_app/views.py
+++ my_first_app/views.py
@@ -4,3 +4,7 @@ from django.http import HttpResponse
 # Create your views here.
 def index(request):
     return HttpResponse("Welcome to My First Django App!")
+def home(request):
+    return HttpResponse("Hello World")
+def about(request):
+    return HttpResponse("This is the about page")
